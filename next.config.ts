9e30674eca5e70270b6ed411084089db1b9413d6
zironpro <dev@zironpro.ae>
2026-05-16 10:36:29 +0400

    chore(config): enable react compiler and experimental turbopack caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactCompiler: true,
+
+	experimental: {
+		turbopackFileSystemCacheForBuild: true,
+		turbopackFileSystemCacheForDev: true,
+	},
 };
 
 export default nextConfig;
